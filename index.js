33364b6cfff966207525ebb2a9bda536e3a55a8a
Zyenop <125141058+Zyenop@users.noreply.github.com>
2023-09-02 10:55:33 -1200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+const accessKey = "fxKyCO06TKEhKAjq9IS0SkduwnfqWw7Sn9S7cQjrUHE";
+
+const formEl = document.querySelector("form");
+
+const searchInputEl = document.getElementById("search-input");
+
+const searchResultEl = document.querySelector(".search-results");
+
+const showMoreBtnEl = document.getElementById("show-more-button");
+
+let inputData = "";
+let page = 1;
+
+async function searchImages(inputData) {
+  inputData = searchInputEl.value;
+
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+
+  const response = await fetch(url);
+
+  const data = await response.json();
+
+  if(page === 1) {
+    searchResultEl.innerHTML = "";
+  }
+
+  const results = data.results;
+
+  results.map((result) => {
+    const imageWrapper = document.createElement("div");
+    imageWrapper.classList.add("search-result");
+    const image = document.createElement("img");
+    image.src = result.urls.small
+    image.alt = result.alt_description;
+
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html
+    imageLink.target = "_blank"
+    imageLink.textContent = result.alt_description;
+
+    imageWrapper.appendChild(image);
+    imageWrapper.appendChild(imageLink);
+    searchResultEl.appendChild(imageWrapper)
+
+  })
+
+  page++
+
+  if (page > 1) {
+    showMoreBtnEl.style.display = "block";
+  }
+}
+
+formEl.addEventListener('submit', (event) => {
+  event.preventDefault();
+  page = 1
+  searchImages();
+})
+
+showMoreBtnEl.addEventListener('click', ()=> {
+  searchImages()
+})
